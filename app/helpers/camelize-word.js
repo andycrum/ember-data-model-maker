aabefc989f2bee95a175042d586b73a9ec14ef07
Andy Crum <andy.crum@gmail.com>
2014-05-27 20:58:19 -0700

    gh-pages commit (dist/ folder, basically)

--- app/helpers/camelize-word.js
+++
@@ -1,4 +0,0 @@
-// Camelizes
-export default Ember.Handlebars.makeBoundHelper(function(word) {
-  return Ember.String.camelize(word);
-});
--- app/utils/constants.js
+++
@@ -1,9 +0,0 @@
-// Define some constants
-export default {
-  ADAPTER_OPTIONS: ['DS.RESTAdapter', 'DS.ActiveModelAdapter'],
-  TYPE_OPTIONS: ['string', 'number', 'boolean', 'date', 'hasMany', 'belongsTo'],
-  PLACEHOLDER_STRING: 'foo',
-  PLACEHOLDER_NUMBER: 123,
-  PLACEHOLDER_BOOLEAN: true,
-  PLACEHOLDER_DATE: new Date()
-};
